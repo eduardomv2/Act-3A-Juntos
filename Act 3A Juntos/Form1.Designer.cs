d9babc61b8c617353283b109773624f5142a9410
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-04-29 07:55:49 -0600

    Agregar archivos de proyecto.

---
+++ Act 3A Juntos/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Act_3A_Juntos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddLoan = new System.Windows.Forms.Button();
+            this.lblLoanOutput = new System.Windows.Forms.Label();
+            this.txtLoanInput = new System.Windows.Forms.TextBox();
+            this.btnShowLoans = new System.Windows.Forms.Button();
+            this.btnFindAuthor = new System.Windows.Forms.Button();
+            this.numAuthorIndex = new System.Windows.Forms.NumericUpDown();
+            this.lblAuthorOutput = new System.Windows.Forms.Label();
+            this.btnAddUser = new System.Windows.Forms.Button();
+            this.txtUserInput = new System.Windows.Forms.TextBox();
+            this.numUserIndex = new System.Windows.Forms.NumericUpDown();
+            this.lblUserOutput = new System.Windows.Forms.Label();
+            this.btnReadUser = new System.Windows.Forms.Button();
+            this.btnDeleteUser = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numAuthorIndex)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUserIndex)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAddLoan
+            // 
+            this.btnAddLoan.Location = new System.Drawing.Point(47, 141);
+            this.btnAddLoan.Name = "btnAddLoan";
+            this.btnAddLoan.Size = new System.Drawing.Size(75, 23);
+            this.btnAddLoan.TabIndex = 0;
+            this.btnAddLoan.Text = "button1";
+            this.btnAddLoan.UseVisualStyleBackColor = true;
+            this.btnAddLoan.Click += new System.EventHandler(this.btnAddLoan_Click);
+            // 
+            // lblLoanOutput
+            // 
+            this.lblLoanOutput.AutoSize = true;
+            this.lblLoanOutput.Location = new System.Drawing.Point(87, 224);
+            this.lblLoanOutput.Name = "lblLoanOutput";
+            this.lblLoanOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblLoanOutput.TabIndex = 1;
+            this.lblLoanOutput.Text = "label1";
+            // 
+            // txtLoanInput
+            // 
+            this.txtLoanInput.Location = new System.Drawing.Point(62, 32);
+            this.txtLoanInput.Name = "txtLoanInput";
+            this.txtLoanInput.Size = new System.Drawing.Size(381, 20);
+            this.txtLoanInput.TabIndex = 2;
+            // 
+            // btnShowLoans
+            // 
+            this.btnShowLoans.Location = new System.Drawing.Point(143, 141);
+            this.btnShowLoans.Name = "btnShowLoans";
+            this.btnShowLoans.Size = new System.Drawing.Size(75, 23);
+            this.btnShowLoans.TabIndex = 3;
+            this.btnShowLoans.Text = "button1";
+            this.btnShowLoans.UseVisualStyleBackColor = true;
+            this.btnShowLoans.Click += new System.EventHandler(this.btnShowLoans_Click);
+            // 
+            // btnFindAuthor
+            // 
+            this.btnFindAuthor.Location = new System.Drawing.Point(244, 141);
+            this.btnFindAuthor.Name = "btnFindAuthor";
+            this.btnFindAuthor.Size = new System.Drawing.Size(75, 23);
+            this.btnFindAuthor.TabIndex = 4;
+            this.btnFindAuthor.Text = "button1";
+            this.btnFindAuthor.UseVisualStyleBackColor = true;
+            this.btnFindAuthor.Click += new System.EventHandler(this.btnFindAuthor_Click);
+            // 
+            // numAuthorIndex
+            // 
+            this.numAuthorIndex.Location = new System.Drawing.Point(475, 33);
+            this.numAuthorIndex.Name = "numAuthorIndex";
+            this.numAuthorIndex.Size = new System.Drawing.Size(120, 20);
+            this.numAuthorIndex.TabIndex = 5;
+            // 
+            // lblAuthorOutput
+            // 
+            this.lblAuthorOutput.AutoSize = true;
+            this.lblAuthorOutput.Location = new System.Drawing.Point(262, 224);
+            this.lblAuthorOutput.Name = "lblAuthorOutput";
+            this.lblAuthorOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblAuthorOutput.TabIndex = 6;
+            this.lblAuthorOutput.Text = "label1";
+            // 
+            // btnAddUser
+            // 
+            this.btnAddUser.Location = new System.Drawing.Point(343, 141);
+            this.btnAddUser.Name = "btnAddUser";
+            this.btnAddUser.Size = new System.Drawing.Size(75, 23);
+            this.btnAddUser.TabIndex = 7;
+            this.btnAddUser.Text = "button1";
+            this.btnAddUser.UseVisualStyleBackColor = true;
+            this.btnAddUser.Click += new System.EventHandler(this.btnAddUser_Click);
+            // 
+            // txtUserInput
+            // 
+            this.txtUserInput.Location = new System.Drawing.Point(62, 67);
+            this.txtUserInput.Name = "txtUserInput";
+            this.txtUserInput.Size = new System.Drawing.Size(381, 20);
+            this.txtUserInput.TabIndex = 8;
+            // 
+            // numUserIndex
+            // 
+            this.numUserIndex.Location = new System.Drawing.Point(475, 68);
+            this.numUserIndex.Name = "numUserIndex";
+            this.numUserIndex.Size = new System.Drawing.Size(120, 20);
+            this.numUserIndex.TabIndex = 9;
+            // 
+            // lblUserOutput
+            // 
+            this.lblUserOutput.AutoSize = true;
+            this.lblUserOutput.Location = new System.Drawing.Point(408, 224);
+            this.lblUserOutput.Name = "lblUserOutput";
+            this.lblUserOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblUserOutput.TabIndex = 10;
+            this.lblUserOutput.Text = "label1";
+            // 
+            // btnReadUser
+            // 
+            this.btnReadUser.Location = new System.Drawing.Point(493, 141);
+            this.btnReadUser.Name = "btnReadUser";
+            this.btnReadUser.Size = new System.Drawing.Size(75, 23);
+            this.btnReadUser.TabIndex = 11;
+            this.btnReadUser.Text = "button1";
+            this.btnReadUser.UseVisualStyleBackColor = true;
+            this.btnReadUser.Click += new System.EventHandler(this.btnReadUser_Click);
+            // 
+            // btnDeleteUser
+            // 
+            this.btnDeleteUser.Location = new System.Drawing.Point(627, 141);
+            this.btnDeleteUser.Name = "btnDeleteUser";
+            this.btnDeleteUser.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteUser.TabIndex = 12;
+            this.btnDeleteUser.Text = "button1";
+            this.btnDeleteUser.UseVisualStyleBackColor = true;
+            this.btnDeleteUser.Click += new System.EventHandler(this.btnDeleteUser_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDeleteUser);
+            this.Controls.Add(this.btnReadUser);
+            this.Controls.Add(this.lblUserOutput);
+            this.Controls.Add(this.numUserIndex);
+            this.Controls.Add(this.txtUserInput);
+            this.Controls.Add(this.btnAddUser);
+            this.Controls.Add(this.lblAuthorOutput);
+            this.Controls.Add(this.numAuthorIndex);
+            this.Controls.Add(this.btnFindAuthor);
+            this.Controls.Add(this.btnShowLoans);
+            this.Controls.Add(this.txtLoanInput);
+            this.Controls.Add(this.lblLoanOutput);
+            this.Controls.Add(this.btnAddLoan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numAuthorIndex)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUserIndex)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddLoan;
+        private System.Windows.Forms.Label lblLoanOutput;
+        private System.Windows.Forms.TextBox txtLoanInput;
+        private System.Windows.Forms.Button btnShowLoans;
+        private System.Windows.Forms.Button btnFindAuthor;
+        private System.Windows.Forms.NumericUpDown numAuthorIndex;
+        private System.Windows.Forms.Label lblAuthorOutput;
+        private System.Windows.Forms.Button btnAddUser;
+        private System.Windows.Forms.TextBox txtUserInput;
+        private System.Windows.Forms.NumericUpDown numUserIndex;
+        private System.Windows.Forms.Label lblUserOutput;
+        private System.Windows.Forms.Button btnReadUser;
+        private System.Windows.Forms.Button btnDeleteUser;
+    }
+}
+
---
+++ Act 3A Juntos/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Act_3A_Juntos
+{
+    public partial class Form1 : Form
+    {
+        // Rutas de los archivos
+        private const string loansFilePath = "loans.txt"; // Archivo secuencial
+        private const string authorsFilePath = "indexedAuthors.txt"; // Archivo secuencial indexado
+        private const string usersFilePath = "randomUsers.bin"; // Archivo de acceso directo
+
+        // Tamaño del registro para el archivo de acceso directo
+        private const int recordSize = 50;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddLoan_Click(object sender, EventArgs e)
+        {
+            string loanInfo = txtLoanInput.Text;
+
+            using (var writer = new StreamWriter(loansFilePath, true))
+            {
+                writer.WriteLine(loanInfo);
+            }
+
+            txtLoanInput.Clear();
+            lblLoanOutput.Text = "Loan added.";
+
+        }
+
+        private void btnShowLoans_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(loansFilePath))
+            {
+                string content = File.ReadAllText(loansFilePath);
+                lblLoanOutput.Text = "All loans:\n" + content;
+            }
+            else
+            {
+                lblLoanOutput.Text = "Loans file does";
+                lblLoanOutput.Text = "Loans file does not exist.";
+            }
+        }
+
+        private void btnFindAuthor_Click(object sender, EventArgs e)
+        {
+            int index = (int)numAuthorIndex.Value;
+
+            if (File.Exists(authorsFilePath))
+            {
+                var lines = File.ReadAllLines(authorsFilePath);
+                var authorLine = lines.FirstOrDefault(line => line.StartsWith($"{index}:"));
+
+                if (authorLine != null)
+                {
+                    lblAuthorOutput.Text = $"Author at index {index}: {authorLine.Split(':')[1].Trim()}";
+                }
+                else
+                {
+                    lblAuthorOutput.Text = "Author not found at this index.";
+                }
+            }
+            else
+            {
+                lblAuthorOutput.Text = "Authors file does not exist.";
+            }
+        }
+
+        private void btnAddUser_Click(object sender, EventArgs e)
+        {
+            int index = (int)numUserIndex.Value;
+            string userInfo = txtUserInput.Text;
+
+            byte[] data = new byte[recordSize];
+            byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(userInfo);
+
+            Array.Copy(textBytes, data, Math.Min(recordSize, textBytes.Length));
+
+            using (var stream = new FileStream(usersFilePath, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                stream.Seek(index * recordSize, SeekOrigin.Begin);
+                stream.Write(data, 0, recordSize);
+            }
+
+            txtUserInput.Clear();
+            lblUserOutput.Text = $"User info added at index {index}.";
+        }
+
+        private void btnReadUser_Click(object sender, EventArgs e)
+        {
+            int index = (int)numUserIndex.Value;
+
+            if (File.Exists(usersFilePath))
+            {
+                using (var stream = new FileStream(usersFilePath, FileMode.Open, FileAccess.Read))
+                {
+                    stream.Seek(index * recordSize, SeekOrigin.Begin);
+
+                    byte[] data = new byte[recordSize];
+                    stream.Read(data, 0, recordSize);
+
+                    string userInfo = System.Text.Encoding.UTF8.GetString(data).Trim('\0');
+
+                    lblUserOutput.Text = $"User at index {index}: {userInfo}";
+                }
+            }
+            else
+            {
+                lblUserOutput.Text = "Users file does not exist.";
+            }
+        }
+
+        private void btnDeleteUser_Click(object sender, EventArgs e)
+        {
+            int index = (int)numUserIndex.Value;
+
+            if (File.Exists(usersFilePath))
+            {
+                using (var stream = new FileStream(usersFilePath, FileMode.Open, FileAccess.Write))
+                {
+                    stream.Seek(index * recordSize, SeekOrigin.Begin);
+
+                    byte[] emptyData = new byte[recordSize];
+                    stream.Write(emptyData, 0, recordSize);
+
+                    lblUserOutput.Text = $"Deleted user at index {index}.";
+                }
+            }
+            else
+            {
+                lblUserOutput.Text = "Users file does not exist.";
+            }
+        }
+    }
+    
+}
